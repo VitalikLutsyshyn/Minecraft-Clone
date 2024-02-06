dad683290eb0d4df04b0873d4d96c9bf9375c2b2
Vitalik <vitalii.lutsyshyn.liceum2@gmail.com>
2024-02-06 16:48:36 +0200

    up

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from ursina import*
 app = Ursina()#cтвореня гри
 
 class Block(Entity):
-    def __init__
-
-block = Entity(model="assets\minecraft-grass-block\source\Minecraft_Grass_Block_OBJ\Grass_Block",
+    def __init__(self,**kwargs):
+        super().__init__(model="assets\minecraft-grass-block\source\Minecraft_Grass_Block_OBJ\Grass_Block",
                texture="assets\minecraft-grass-block\source\Minecraft_Grass_Block_OBJ\Grass_Block_TEX.png",
                scale=2,
-            position=(6,-5,100))
+            position=(6,-5,100),**kwargs)
+
+
+
+block = Block()
 block.y = 10
 
 
